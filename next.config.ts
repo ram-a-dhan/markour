5d01b28a84d19dadc1709f0396930637fd79f0f1
ram-a-dhan <ram.adhan@outlook.com>
2026-08-18 19:17:17 +0700

    feat: bootstrapping mantine framework

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["dev.markour.space"],
+  experimental: {
+    optimizePackageImports: [
+      "@mantine/core",
+      "@mantine/hooks",
+    ],
+  },
 };
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
+    "postcss-preset-mantine": {},
+    "postcss-simple-vars": {},
   },
 };
 
