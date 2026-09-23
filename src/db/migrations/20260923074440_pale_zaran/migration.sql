20c0a17bdbbb49a1f868a1006240eeb45355cffb
ram-a-dhan <ram.adhan@outlook.com>
2026-09-23 15:54:16 +0700

    chore: redefine migration

--- src/db/migrations/20260923074440_pale_zaran/migration.sql
+++ src/db/migrations/20260923074440_pale_zaran/migration.sql
@@ -6,12 +6,33 @@ CREATE TABLE `note_tags` (
 	CONSTRAINT `fk_note_tags_tag_id_tags_id_fk` FOREIGN KEY (`tag_id`) REFERENCES `tags`(`id`) ON DELETE CASCADE
 );
 --> statement-breakpoint
+CREATE TABLE `notes` (
+	`id` text PRIMARY KEY,
+	`user_id` text NOT NULL,
+	`title` text DEFAULT '' NOT NULL,
+	`content` text DEFAULT '' NOT NULL,
+	`pinned` integer DEFAULT false NOT NULL,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL,
+	`deleted_at` integer,
+	`version` integer DEFAULT 1 NOT NULL,
+	CONSTRAINT `fk_notes_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+);
+--> statement-breakpoint
 CREATE TABLE `tags` (
 	`id` text PRIMARY KEY,
 	`user_id` text NOT NULL,
 	`name` text NOT NULL,
 	`created_at` integer NOT NULL,
 	CONSTRAINT `fk_tags_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY,
+	`name` text DEFAULT '' NOT NULL,
+	`email` text NOT NULL UNIQUE,
+	`picture` text DEFAULT '' NOT NULL,
+	`created_at` integer NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `tags_user_name_idx` ON `tags` (`user_id`,`name`);
